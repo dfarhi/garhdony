9908e98cd4a95925ab6e23ef8ea66462ded3e054
dfarhi <32961271+dfarhi@users.noreply.github.com>
2023-07-29 21:19:57 -0700

    NPC editing tests

--- garhdony_app/tests/test_views_game_design.py
+++ garhdony_app/tests/test_views_game_design.py
@@ -1,12 +1,8 @@
     # Untested urls.py:
-    # url(r'^writing/([^/]+)/sheetsgrid/$', garhdony_app.views_game_design.writing_game_sheets_grid,
-    #     name='game_writer_sheets_grid'),
     # url(r'^writing/([^/]+)/timeline/$', garhdony_app.views_game_design.writing_game_timeline,
     #     name='game_writer_timeline'),
     # url(r'^writing/([^/]+)/sheetsgrid/modify/$', garhdony_app.views_game_design.sheets_grid_modify,
     #     name='sheets_grid_modify'),
-    # url(r'^writing/([^/]+)/characters/table/$', garhdony_app.views_game_design.writing_characters_table,
-    #     name='game_writer_characters_table'),
     # url(r'^writing/([^/]+)/sheet/new/$', garhdony_app.views_game_design.writer_new_sheet, name='new_sheet'),
     # url(r'^writing/([^/]+)/sheet/delete/$', garhdony_app.views_game_design.delete_sheet, name='delete_sheet'),
     # url(r'^writing/([^/]+)/character/new/$', garhdony_app.views_game_design.new_character, name='new_character'),
@@ -19,14 +15,18 @@
     # url(r'^writing/([^/]+)/search/$', garhdony_app.views_game_design.search, name='search'),
     # url(r'^writing/([^/]+)/recent_changes/$', garhdony_app.views_game_design.recent_changes, name='recent_changes'),
 
+from io import BytesIO
 import os
 import shutil
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from garhdony_app import models
 from garhdony_app.assign_writer_game import assign_writer_game
 from garhdony_app.tests.setup_test_db import setup_test_db
 from django.contrib.auth.models import User, Group
 
+def inline_edit_buttom_html(field):
+    return f"""<form action="" method="get" style="display:inline"><input type="hidden" name="Edit" value="{field}"><input class="edit_button" type="submit" value=Edit></form>"""
 
 class GameDesignNotLoggedInTest(TestCase):
     def test_not_logged_in(self):
@@ -122,7 +122,9 @@ class NongameDesignViewsTest(TestCase):
         self.assertEqual(some_character.genderized_names.filter(male="M").count(), 0)
         self.assertEqual(some_characters_clone.genderized_names.filter(male="M").count(), 1)
         
-class GameDesignViewsTest(TestCase):
+# Make sure to use a separate media root for each test case function call.
+@override_settings(DEFAULT_FILE_STORAGE='django.core.files.storage.InMemoryStorage', MEDIA_ROOT='media/test_media')
+class GameDesignViewsTestCase(TestCase):
     def setUp(self) -> None:
         setup_test_db()
         self.game = models.GameInstance.objects.get(name="TestGame")
@@ -142,6 +144,7 @@ class GameDesignViewsTest(TestCase):
         self.assertContains(response, "Game Search")
         self.assertContains(response, "Website Bugs")
 
+class GameDesignViewsTest(GameDesignViewsTestCase):
     def test_writing_game_home(self):
         response = self.client.get(f"/writing/{self.game.name}/")
         self.assertEqual(response.status_code, 200)
@@ -171,7 +174,7 @@ class GameDesignViewsTest(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_writing_sheets_grid(self):
-        response = self.client.get(f"/writing/{self.game.name}/sheets/")
+        response = self.client.get(f"/writing/{self.game.name}/sheetsgrid/")
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.game.name)
         for sheet in models.Sheet.objects.filter(game=self.game):
@@ -182,3 +185,195 @@ class GameDesignViewsTest(TestCase):
         # check sidebar
         self.assert_writer_game_leftbar(response)
     
+    def test_writing_characters_grid(self):
+        response = self.client.get(f"/writing/{self.game.name}/characters/table/")
+        self.assertEqual(response.status_code, 200)
+        # check sidebar
+        self.assert_writer_game_leftbar(response)
+
+        self.assertContains(response, self.game.name)
+        for character in models.PlayerCharacter.objects.filter(game=self.game):
+            self.assertContains(response, character.first_name())
+            self.assertContains(response, character.last_name)
+
+class NPCEditingTest(GameDesignViewsTestCase):
+    def test_writing_npc_page_loads(self):
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        response = self.client.get(f"/writing/{self.game.name}/NPC/{character.id}/")
+        self.assertEqual(response.status_code, 200)
+        # check sidebar
+        self.assert_writer_game_leftbar(response)
+
+        self.assertContains(response, character.first_name())
+        self.assertContains(response, character.last_name)
+        self.assertContains(response, character.notes)
+
+        # edit forms
+        self.assertContains(response, inline_edit_buttom_html("notes"), html=True)
+        self.assertContains(response, inline_edit_buttom_html("photo"), html=True)
+        self.assertContains(response, inline_edit_buttom_html("Metadata"), html=True)
+
+    def test_writing_npc_page_edit_notes(self):
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        # Check the edit form loads
+        response = self.client.get(f"/writing/{self.game.name}/NPC/{character.id}/", {"Edit": "notes"})
+        self.assertEqual(response.status_code, 200)
+        # check sidebar
+        self.assert_writer_game_leftbar(response)
+
+        # Check there's a form with a field for notes
+        self.assertContains(response, "id_notes")
+
+        # submit a change
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", {"Save": "notes", "notes": "New Notes"})
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.notes.render(), "New Notes")
+
+    def test_writing_npc_page_edit_photo(self):
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        # Check the edit form loads
+        response = self.client.get(f"/writing/{self.game.name}/NPC/{character.id}/", {"Edit": "photo"})
+        self.assertEqual(response.status_code, 200)
+        # Check there's a form with a field for photo
+        self.assertContains(response, "id_photo")
+
+        # submit a change
+        f = BytesIO()
+        from PIL import Image
+        image = Image.new("RGB", (100, 100))
+        image.save(f, 'png')
+        f.seek(0)
+        photo = SimpleUploadedFile("test.png", f.read(), content_type="image/png")
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", {"Save": "photo", "photo": photo})
+
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.photo.open().read(), photo.open().read())
+
+        # check redirect
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f"/writing/{self.game.name}/NPC/{character.id}/")
+
+    def test_writing_npc_page_edit_metadata_name(self):
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        # Check the edit form loads
+        response = self.client.get(f"/writing/{self.game.name}/NPC/{character.id}/", {"Edit": "Metadata"})
+        self.assertEqual(response.status_code, 200)
+        # check sidebar
+        self.assert_writer_game_leftbar(response)
+
+        # Check the form fields
+        self.assertContains(response, "id_first_male")
+        self.assertContains(response, "id_first_female")
+        self.assertContains(response, "id_last_name")
+        self.assertContains(response, "id_title")
+        # button to add new title. Not sure how to check if clicking it does anything.
+        self.assertTrue("plus.jpeg" in response.content.decode('utf-8'))
+
+        # submit a change
+        new_title = models.GenderizedKeyword.objects.filter(category="title").first()
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", 
+                                    {"Save": "Metadata",
+                                     "first_male": "NewFirstMale",
+                                     "first_female": "NewFirstFemale",
+                                     "last_name": "NewLastName",
+                                     "gender_field": "F",
+                                     "title_obj": new_title.id
+                                     })
+        self.assertEqual(response.status_code, 302)
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.first_name_obj.male, "NewFirstMale")
+        self.assertEqual(character.first_name_obj.female, "NewFirstFemale")
+        self.assertEqual(character.last_name, "NewLastName")
+        self.assertEqual(character.gender(), "F")
+        self.assertEqual(character.title_obj, new_title)
+        self.assertEqual(character.first_name(), "NewFirstFemale")
+
+    def post_dict(self, gender_field, gender_linked_pc = None):
+        result = {"Save": "Metadata",
+            "first_male": "NewFirstMale",
+            "first_female": "NewFirstFemale",
+            "last_name": "NewLastName",
+            "gender_field": gender_field,
+            "title_obj": "",  # no title
+        }
+        if gender_linked_pc:
+            result['gender_linked_pc'] = gender_linked_pc.id
+        return result
+
+    def test_writing_npc_page_genders_invalid_MF(self):
+        """ Test setting gender to M or F while also setting gender_linked_pc"""
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        old_first_name = character.first_name()
+        other_character = models.PlayerCharacter.objects.filter(game=self.game).first()
+
+        for g in ["M", "F"]:
+            response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict(g, gender_linked_pc=other_character))
+            # Check that it didn't crash
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "For fixed gender, set blank PC")
+            # Check that it didn't save
+            character = models.NonPlayerCharacter.objects.get(id=character.id)
+            self.assertEqual(character.first_name(), old_first_name)
+
+    def test_writing_npc_page_genders_invalid_OPEQ(self):
+        """ Test setting gender to OP or EQ while not setting gender_linked_pc"""
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        old_first_name = character.first_name()
+        for g in ["OP", "EQ"]:
+            response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict(g, gender_linked_pc=None))
+            # Check that it didn't crash
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "Choose a PC")
+            # Check that it didn't save
+            character = models.NonPlayerCharacter.objects.get(id=character.id)
+            self.assertEqual(character.first_name(), old_first_name)
+
+    def test_writing_npc_page_genders_valid_MF(self):
+        """ Test setting gender to M or F correctly"""
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict("F"))
+        self.assertEqual(response.status_code, 302)
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "F")
+        self.assertEqual(character.first_name(), "NewFirstFemale")
+
+        self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict("M"))
+        self.assertEqual(response.status_code, 302)
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "M")
+        self.assertEqual(character.first_name(), "NewFirstMale")
+
+    def test_writing_npc_page_genders_valid_EQ(self):
+        """ Test setting gender to EQ"""
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        other_character = models.PlayerCharacter.objects.filter(game=self.game).first()
+
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict("EQ", gender_linked_pc=other_character))
+        self.assertEqual(response.status_code, 302)
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        other_character.default_gender = "M"
+        other_character.save()
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "M")
+        other_character.default_gender = "F"
+        other_character.save()
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "F")
+                         
+    def test_writing_npc_page_genders_valid_OP(self):
+        """ Test setting gender to OP"""
+        character = models.NonPlayerCharacter.objects.filter(game=self.game).first()
+        other_character = models.PlayerCharacter.objects.filter(game=self.game).first()
+
+        response = self.client.post(f"/writing/{self.game.name}/NPC/{character.id}/", self.post_dict("OP", gender_linked_pc=other_character))
+        self.assertEqual(response.status_code, 302)
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        other_character.default_gender = "F"
+        other_character.save()
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "M")
+        other_character.default_gender = "M"
+        other_character.save()
+        character = models.NonPlayerCharacter.objects.get(id=character.id)
+        self.assertEqual(character.gender(), "F")
